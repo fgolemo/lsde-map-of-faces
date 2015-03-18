753fa1b17b502097739e844dd6b38c968e4628f7
tilldoe <till.doehmen@web.de>
2015-03-18 15:22:38 +0100

    fixed command line argument parsing and added lat long parsing

--- openimaj-test/src/main/java/vu/nl/lsde/group01/FacesTest.java
+++ openimaj-test/src/main/java/vu/nl/lsde/group01/FacesTest.java
@@ -4,17 +4,12 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,9 +17,7 @@ import java.util.Map;
 import org.apache.commons.compress.compressors.CompressorException;
 import org.apache.commons.compress.compressors.CompressorInputStream;
 import org.apache.commons.compress.compressors.CompressorStreamFactory;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.Text;
@@ -34,13 +27,9 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
-import org.kohsuke.args4j.CmdLineException;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.Option;
 import org.openimaj.feature.FeatureVector;
 import org.openimaj.hadoop.mapreduce.TextBytesJobUtil;
 import org.openimaj.hadoop.sequencefile.MetadataConfiguration;
-import org.openimaj.hadoop.sequencefile.SequenceFileUtility;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
 import org.openimaj.image.colour.Transforms;
@@ -52,26 +41,16 @@ import org.openimaj.io.IOUtils;
 public class FacesTest extends Configured implements Tool {
     private static Logger logger = Logger.getLogger(FacesTest.class);
     
-
-    @Option(name = "-input", aliases="-i", required=true, usage="Set the input path(s) or uri(s)")
-    protected static Collection<String> input;
+    protected static String input;
     
-    @Option(name = "-output", aliases="-o", required=true, usage="Set the output location")
     protected static String output;
     
-    @Option(name = "--binary", aliases="-b", required=false, usage="Set output mode to binary")
-    protected static boolean binary = false;
-    
     public void prepare(String[] args){
-        final CmdLineParser parser = new CmdLineParser(this);
-        try {
-            parser.parseArgument(args);
-        } catch (final CmdLineException e) {
-            System.err.println(e.getMessage());
-            System.err.println("Usage: hadoop jar test.jar [options...] [files...]");
-            parser.printUsage(System.err);
-
-            System.exit(1);
+        if(args.length!=2){
+            System.out.println("input and output parameter are missing. call \"java -jar .jar 'input' 'ouput'\" to run it correctly.");
+        }else{
+            input = args[0];
+            output = args[1];
         }
     }
     
@@ -143,11 +122,12 @@ public class FacesTest extends Configured implements Tool {
                     FeatureVector fv = mode.getFeatureVector(fd.detectFaces(Transforms.calculateIntensityNTSC(img)), img);
 
                     ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                    if (FacesTest.binary)
-                        IOUtils.writeBinary(baos, fv);
-                    else
-                        IOUtils.writeASCII(baos, fv);
-
+                    IOUtils.writeBinary(baos, fv);
+                    
+                    double lon = Double.parseDouble(columns[10].trim());
+                    double lat = Double.parseDouble(columns[11].trim());
+                    
+                    
                     context.write(key, new BytesWritable(baos.toByteArray()));
                     in.close();
                 } catch (Exception e) {
@@ -178,16 +158,17 @@ public class FacesTest extends Configured implements Tool {
 
     @Override
     public int run(String[] args) throws Exception {
+        prepare(args);
         Map<String,String> metadata = new HashMap<String,String>();
-        metadata.put(MetadataConfiguration.CONTENT_TYPE_KEY, "application/globalfeature-HAAR_FACES-" + (binary? "bin" : "ascii" ));
-        metadata.put("clusterquantiser.filetype", (binary ? "bin" : "ascii" ));
+        metadata.put(MetadataConfiguration.CONTENT_TYPE_KEY, "application/globalfeature-HAAR_FACES-" + ("ascii" ));
+        metadata.put("clusterquantiser.filetype", ("ascii" ));
         
         List<Path> allPaths = new ArrayList<Path>();
-        for (String p : input) {
-            allPaths.addAll(Arrays.asList(SequenceFileUtility.getFilePaths(p, "part")));
+        //allPaths.addAll(Arrays.asList(SequenceFileUtility.getFilePaths(input, "part")));
+        for(Path path: allPaths){
+            System.out.println(path);
         }
-        
-        Job job = TextBytesJobUtil.createJob(allPaths, new Path(output), metadata, this.getConf());
+        Job job = TextBytesJobUtil.createJob(new Path("resources/input/yfcc100m_dataset-0.bz2"), new Path(output), metadata, this.getConf());
         job.setJarByClass(this.getClass());
         job.setMapperClass(FacesMapper.class);
         job.setReducerClass(FacesReducer.class);
